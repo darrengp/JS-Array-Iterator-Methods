6617ee532a3dcf6b72e69fa0565fc4b32bc9e6bc
darrengp <ceo.darrenperry@gmail.com>
2021-02-02 10:15:49 -0800

    class lab begin

--- js/code-along.js
+++ js/code-along.js
@@ -1,1 +1,21 @@
 // a space for your code along notes 🏄‍♀️
+
+// Array Methods ////////-----------
+
+let myFruits = ["banana", "mango", "apple", "pear", "strawberry", "rasberry"];
+
+myFruits.push("blackberry");
+console.log(myFruits);
+
+////// foreach() /////// ------------------------- does not edit the original array
+myFruits.forEach(function (fruit) {
+  console.log(fruit);
+});
+
+/// map() ////////////// -------------------------- returns a new array mapped from the orinal one always needs a return statement
+
+let myFavoriteFruit = myFruits.map(function (fruit) {
+  return `One of my favortie fruit is ${fruit}`;
+});
+
+console.log(myFavoriteFruit);
